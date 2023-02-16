7dc1e04865eddfa64bfde0f11a8d8f7c90de108f
jsthemes68 <jsthemes68@gmail.com>
2023-02-16 15:04:56 +0700

    update
    update

--- src/common/with-router.js
+++ src/common/with-router.js
@@ -1,5 +1,5 @@
-import { useLocation, useNavigate, useParams } from "react-router-dom";
-
+import React from "react";
+import {useLocation, useNavigate, useParams } from "react-router-dom";
 export const withRouter = (Component) => {
   function ComponentWithRouterProp(props) {
     let location = useLocation();
